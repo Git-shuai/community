f73e134cdd6ad82f210186492d34f996cb9bf383
tian <1873461847@qq.com>
2020-06-10 08:27:00 +0800

    init repo

---
+++ src/main/java/life/tain/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package life.tain.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author FTS36
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
