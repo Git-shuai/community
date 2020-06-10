6d5fd2a882a853871d48d0be5b3abfac402d4d1d
shuai <1873461847@qq.com>
2020-06-10 22:38:12 +0800

    github登录

--- src/main/java/life/tain/community/controller/AuthorizeController.java
+++ src/main/java/life/tain/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.tain.community.dto.AccessTokenDTO;
 import life.tain.community.dto.GitHubUser;
 import life.tain.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,20 +20,29 @@ public class AuthorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("Iv1.160de4e8641db070");
-        accessTokenDTO.setClient_secret("2e3bb0914c73af54b801e493162ca637300f3a13");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = gitHubProvider.getUser(accessToken);
 
-        System.out.println("id="+user.getId());
+        System.out.println("name="+user.getName());
         gitHubProvider.getAccessToken(accessTokenDTO);
         return "index";
     }
